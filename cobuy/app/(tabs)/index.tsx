9f5cd74068270b212a78ea6b5f3732d3de1a4049
awesomePei <momotsai910@gmail.com>
2025-05-10 11:34:14 +0800

    added search commodity function

--- cobuy/app/(tabs)/index.tsx
+++ cobuy/app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
-import { Text, SafeAreaView, TouchableOpacity, FlatList } from "react-native";
+import { Text, SafeAreaView, TouchableOpacity, FlatList, TextInput } from "react-native";
 import {Link} from 'expo-router';
+import { useState } from "react";
 
 // 先放寫死的假資料，之後可能要用 API fetch
 const products = [
@@ -9,12 +10,24 @@ const products = [
 ]
 
 export default function Index() {
+  const [ searchTerm, setSearchTerm ] = useState('');
+  const filteredProducts = products.filter((item) =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+
   return (
     <SafeAreaView className="flex-1 justify-center items-center bg-primary">
       <Text className="text-3xl text-secondary font-bold mb-4">Welcome to CoBuy bitch</Text>
+      <TextInput
+        placeholder="搜尋商品名稱"
+        value={searchTerm}
+        onChangeText={setSearchTerm}
+        className="bg-primary text-secondary rounded p-2 mb-4 border border-block"
+      />
       <Text className="text-xl font-bold mb-4 text-secondary">商品列表</Text>
       <FlatList
-        data={products}
+        data={filteredProducts}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <Link
